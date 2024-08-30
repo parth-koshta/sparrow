57f61370d72b65b1c2a07c3908244a6fc6b29b91
Parth <parth@urbanpiper.com>
2024-08-31 01:51:42 +0530

    added viper

--- main.go
+++ main.go
@@ -7,29 +7,29 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/parth-koshta/sparrow/api"
 	db "github.com/parth-koshta/sparrow/db/sqlc"
-)
-
-const (
-	dbSource      = "postgresql://root:secret@localhost:5432/sparrow-dev?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/parth-koshta/sparrow/util"
 )
 
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("could not load env config", err)
+	}
 	ctx := context.Background()
-	conn, err := pgxpool.New(ctx, dbSource)
+	conn, err := pgxpool.New(ctx, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
 	err = conn.Ping(ctx)
 	if err != nil {
-		log.Fatal("cannot ping db: ", err)
+		log.Fatal("cannot ping db:", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
