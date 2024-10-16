589fae682480e827ec5df8173bfd5131800739d0
Parth <parth@urbanpiper.com>
2024-10-16 22:06:59 +0530

    start.sh update

--- start.sh
+++ start.sh
@@ -2,9 +2,17 @@
 
 set -e
 
+echo "Checking if /app/app.env exists"
+if [ -f /app/app.env ]; then
+    echo "File /app/app.env exists. Sourcing the environment variables."
+    source /app/app.env
+    echo "source: $DB_SOURCE"
+else
+    echo "File /app/app.env does not exist."
+    exit 1
+fi
+
 echo "run database migrations"
-source /app/app.env
-echo "source: $DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the application"
