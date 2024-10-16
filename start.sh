917e9ba1f206ddd0c700923535a284f846366244
Parth <parth@urbanpiper.com>
2024-10-16 21:08:08 +0530

    print DB_SOURCE

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "run database migrations"
 source /app/app.env
+echo "source: $DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the application"
